0900109b4da26cf6908eee4bd289e018b73d94bd
martinpriest <marcin.kaplan@gmail.com>
2024-03-07 07:51:17 +0100

    Add Book factory class and add BORROWED BookStatus option

--- app/Enums/BookStatus.php
+++ app/Enums/BookStatus.php
@@ -6,11 +6,13 @@ enum BookStatus: string
 {
     case AVAILABLE = 'AVAILABLE';
     case NOT_AVAILABLE = 'NOT_AVAILABLE';
+    case BORROWED = 'BORROWED';
 
     public function isAvailable(): bool
     {
         return match ($this) {
             self::AVAILABLE => true,
+            self::BORROWED => false,
             self::NOT_AVAILABLE => false,
         };
     }
---
+++ database/factories/BookFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\BookStatus;
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->realText(64),
+            'author' => $this->faker->name(),
+            'publication_date' => $this->faker->date(),
+            'publishing_house' => $this->faker->company(),
+            'status' => $this->faker->randomElement([BookStatus::AVAILABLE->value, BookStatus::NOT_AVAILABLE->value]),
+            'customer_id' => null,
+        ];
+    }
+
+    public function available()
+    {
+        return $this->state(
+            fn () => [
+                'status' => BookStatus::AVAILABLE->value,
+                'customer_id' => null,
+            ]
+        );
+    }
+
+    public function notAvailable()
+    {
+        return $this->state(
+            fn () => [
+                'status' => BookStatus::NOT_AVAILABLE->value,
+            ]
+        );
+    }
+
+    public function borrowed()
+    {
+        return $this->state(
+            fn () => [
+                'status' => BookStatus::BORROWED->value,
+                'customer_id' => Customer::factory(),
+            ]
+        );
+    }
+}
