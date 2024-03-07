8d5c6000a55736f25646e792de95b2f661f9890f
martinpriest <marcin.kaplan@gmail.com>
2024-03-07 07:20:08 +0100

    Add customers and books table migration files

---
+++ database/migrations/2024_03_07_061711_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('status')->default('AVAILABLE')->comment('Book availability status');
+            $table->string('title');
+            $table->string('author');
+            $table->date('publication_date');
+            $table->string('publishing_house');
+            $table->foreignId('customer_id')
+                ->nullable()
+                ->default(null)
+                ->constrained('customers')
+                ->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
