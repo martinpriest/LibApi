3ea84e29a7723048b1a607f68404c58538711406
martinpriest <marcin.kaplan@gmail.com>
2024-03-07 07:35:12 +0100

    Use enum available value in books table migration file

--- database/migrations/2024_03_07_061711_create_books_table.php
+++ database/migrations/2024_03_07_061711_create_books_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\BookStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('status')->default('AVAILABLE')->comment('Book availability status');
+            $table->string('status')
+                ->default(BookStatus::AVAILABLE->value)
+                ->comment('Book availability status');
             $table->string('title');
             $table->string('author');
             $table->date('publication_date');
