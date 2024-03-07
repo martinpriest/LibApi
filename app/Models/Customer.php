40b08995e501d8392ed8c72944a3c58f7023f693
martinpriest <marcin.kaplan@gmail.com>
2024-03-07 07:33:18 +0100

    Add method to get books borrowed by Customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Customer extends Model
 {
@@ -18,4 +19,9 @@ class Customer extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
 }
