9081542954a2d248d86481c6668aac4d2612cff7
martinpriest <marcin.kaplan@gmail.com>
2024-03-07 07:53:40 +0100

    Create books and customer at DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Customer::factory(10)->create();
+        Book::factory(50)->create();
+        Book::factory(5)->notAvailable()->create();
+        Book::factory(5)->borrowed()->create();
     }
 }
